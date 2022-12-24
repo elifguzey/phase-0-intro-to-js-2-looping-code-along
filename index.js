f528b7b3e876e99d9121befe5ac9ea0047627504
Elif Guzey <elifguzey3@gmail.com>
2022-12-23 20:40:15 -0500

    commit tree 66649e58f1b12f742da51df2a4c454f0dde9a779

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+
+  return gifts;
+}
+
+//wrapGifts(gifts);
+
+function writeCards (nameArray , event) {
+    let messages = []
+for (let i = 0; i < nameArray.length; i++) {
+messages.push(`Thank you, ${nameArray[i]}, for the wonderful ${event} gift!`)
+} 
+return messages;
+}
+
+function countDown(number){
+for (let i=number; i>=0; i--) {
+    console.log(i)
+}
+}
+//let countDown = 10;
+//while (countDown > 0) {
+    //console.log(countDown--);
+//}
